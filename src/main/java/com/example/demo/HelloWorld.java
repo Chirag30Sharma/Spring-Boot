20eff364859a78cc7af5d835249e79c540412216
= <=>
2024-01-19 10:15:08 +0530

    added for experiment 2

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,31 +1,46 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
-	@GetMapping("/world")
-	public String get() {
-		return "<b>Hello World</b>";
-	}
 
-	@GetMapping("/{name}")
-	public String getMethodName(@PathVariable String name) {
-		return name;
-	}
-	
-	@PostMapping("/Enter")
-	public String insert(@RequestBody Details ob) {		
-		Details.arr1.add(new Details(ob.number, ob.name));
-		for(Details obd: Details.arr1){
-			System.out.println(obd.name + " " + obd.number);
-		}
-		return "Data Inserted";
-	}
-}		
+    private List<Student> students = new ArrayList<>(); 
+
+    @GetMapping("/world")
+    public String get() {
+        return "<b>Hello World</b>";
+    }
+
+    @GetMapping("/{name}")
+    public String getURL(@PathVariable String name) {
+        return name;
+    }
+
+    @GetMapping("/student")
+    public List<Student> getStudents() {
+        if (students.isEmpty()) {
+            students.add(new Student("Computer", "Network"));
+            students.add(new Student("Spring", "Boot"));
+            students.add(new Student("Operating", "System"));
+        }
+        return students;
+    }
+
+    @PostMapping("/insert")
+    public List<Student> insertStudents(@RequestBody Student student) {
+        students.add(student);
+        return students;
+    }
+
+    @DeleteMapping("/delete/{firstName}/{lastName}")
+    public List<Student> deleteStudent(@PathVariable String firstName, @PathVariable String lastName) {
+        // students.remove(0); To delete the first element of the List.
+        students.removeIf(student -> student.getFirstName().equals(firstName) && student.getLastName().equals(lastName));
+        return students;
+    }
+}
